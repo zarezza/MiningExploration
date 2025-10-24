36a692d636a64b9de22dbb24f98771235a6c1e3d
zarezza <nfahreza01@gmail.com>
2025-10-24 23:02:44 +1000

    update sqls

--- sql/boundary_depth.sql
+++ sql/boundary_depth.sql
@@ -1,9 +1,11 @@
-SELECT 
+SELECT
     BOUNDARY_NAME,
     AGE_TYPE,
     COUNT(*) AS vertex_count,
     ROUND(AVG(DEPTH), 2) AS avg_depth,
-    ROUND(STDDEV(DEPTH), 2) AS depth_variability
+    ROUND(STDDEV(DEPTH), 2) AS depth_variability,
+    ROUND(AVG(LONGITUDE), 6) AS centroid_longitude,
+    ROUND(AVG(LATITUDE), 6) AS centroid_latitude
 FROM base_processed
 WHERE DEPTH_CONFIDENCE IN ('H', 'M')
 GROUP BY BOUNDARY_NAME, AGE_TYPE
--- sql/regional_coverage.sql
+++ sql/regional_coverage.sql
@@ -1,18 +1,22 @@
 WITH regional_bins AS (
-  SELECT 
-    CAST(EASTING / 1000 AS INT) AS region_x,
-    CAST(NORTHING / 1000 AS INT) AS region_y,
+  SELECT
+    CAST(EASTING / 50000 AS INT) AS region_x,
+    CAST(NORTHING / 50000 AS INT) AS region_y,
+    LATITUDE,
+    LONGITUDE,
     DEPTH,
     DEPTH_CONFIDENCE
   FROM base_processed
   WHERE DEPTH IS NOT NULL
 )
-SELECT 
+SELECT
   region_x,
   region_y,
   COUNT(*) AS n_points,
   ROUND(AVG(DEPTH), 2) AS avg_depth,
-  SUM(CASE WHEN DEPTH_CONFIDENCE = 'H' THEN 1 ELSE 0 END) AS high_conf_points
+  SUM(CASE WHEN DEPTH_CONFIDENCE = 'H' THEN 1 ELSE 0 END) AS high_conf_points,
+  ROUND(AVG(LONGITUDE), 6) AS centroid_longitude,
+  ROUND(AVG(LATITUDE), 6) AS centroid_latitude
 FROM regional_bins
 GROUP BY region_x, region_y
 HAVING n_points > 10
